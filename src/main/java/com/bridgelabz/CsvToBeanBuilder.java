fa9209c0e5d1daaf05eb32d87df406d50b72b7da
GENIUS <naikmahesh.naik@gmail.com>
2021-03-22 00:41:49 +0530

    Ability for the analyser

---
+++ src/main/java/com/bridgelabz/CsvToBeanBuilder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class CsvToBeanBuilder {
+    public void withIgnoreLeadingWhiteSpace(boolean b) {
+    }
+
+    public CsvToBean<IndiaCensusCSV> build() {
+        return null;
+    }
+
+    public void withType() {
+
+    }
+}
---
+++ src/main/java/com/bridgelabz/IndiaCensusCSV.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+
+public class IndiaCensusCSV {
+
+    public String state;
+
+    public int population;
+
+    public int areaInSqKm;
+
+    public int densityPerSqKm;
+
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+
+}
