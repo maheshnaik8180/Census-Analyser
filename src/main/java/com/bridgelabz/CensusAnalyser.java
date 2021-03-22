14a8aae6f169f5f9644082068748d95ba7a3d142
GENIUS <naikmahesh.naik@gmail.com>
2021-03-22 12:14:30 +0530

    modify This is Happy test case where the records is matched

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -10,16 +9,16 @@ import java.util.Iterator;
 
 public class CensusAnalyser
 {
-    public <CsvToBean> int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException
     {
         try
         {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType();
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator);
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
             int namOfEateries = 0;
             while (censusCSVIterator.hasNext())
             {
