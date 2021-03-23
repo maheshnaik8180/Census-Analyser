4138e98e4ca535b12a0f90c73c4d68a8c651a3f1
GENIUS <naikmahesh.naik@gmail.com>
2021-03-23 19:57:26 +0530

    givenIndiaStateData_WithWrongType

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -11,6 +11,9 @@ import org.junit.rules.ExpectedException;
         private static final String WRONG_TYPE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.txt";
         private static final String INDIA_STATE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
         private static final String INDIA_STATE_WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
+
+
+
         @Test
         public void givenIndianCensusCSVFileReturnsCorrectRecords() {
             try {
@@ -70,6 +73,21 @@ import org.junit.rules.ExpectedException;
                 Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
             }
         }
+
+
+        @Test
+        public void givenIndiaStateData_WithWrongType_ShouldThrowException() {
+            try {
+                CensusAnalyser censusAnalyser = new CensusAnalyser();
+                ExpectedException exceptionRule = ExpectedException.none();
+                exceptionRule.expect(CensusAnalyserException.class);
+                censusAnalyser.loadIndianStateCode(WRONG_TYPE_CSV_FILE_PATH);
+            } catch (CensusAnalyserException e) {
+                Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+            }
+        }
+
+
 
     }
 
